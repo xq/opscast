9b2d3e089ba861e7d8d75fdca9082289b86f77f3
Thomas Pastinsky <tom@atpay.com>
2016-02-10 00:27:22 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opscast::Application.config.secret_token = 'b42ff2a24db020a2e718288899466a40032c716f36da1968a7979cbd8f614b10c6910bae1df856a30dc0f6ed595572b45afe2e190088950af7cd4491ad4c6f1c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Opscast::Application.config.session_store :cookie_store, key: '_opscast_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Opscast::Application.config.session_store :active_record_store
